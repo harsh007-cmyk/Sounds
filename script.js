1ee4794cdbe6223e4d847e4c4b3ebc6f2b2d9912
Harsh P <harshpmpm07@gmail.com>
2022-07-06 21:23:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var btnsxyz = document.querySelectorAll(".btn");
+
+function stopSounds(){
+  for(var i=0; i<btnsxyz.length;i++){
+    const sound = document.getElementById(btnsxyz[i].innerText);
+    sound.pause();
+     sound.currentTime = 20;
+  }
+}
+function playSound(btn){
+  btn.addEventListener("click", function(){
+    stopSounds();
+    document.getElementById(btn.innerText).play();
+  })
+}
+
+for(var i=0 ; i<btnsxyz.length;i++){
+  var buttonxyz = btnsxyz[i];
+  playSound(buttonxyz);
+}
+document.querySelector(".stop").addEventListener("click", function(){
+  stopSounds();
+})
